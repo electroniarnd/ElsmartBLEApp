369fb0f6ce08698a1207bc46a37cf8f96b4baa16
Abdul Rasheed <rasheeda@electronia.com>
2018-12-20 11:37:47 +0300

    removed class space

--- ElsmartBLEApp/ModelClasses/TerDetails.swift
+++ ElsmartBLEApp/ModelClasses/TerDetails.swift
@@ -5,14 +5,11 @@ class TerDetails {
     var id: Int
     var Address: String?
     var Name: String?
-  
     
     init( id: Int, Name: String?, Address: String? ){
-        
         self.id = id
         self.Name = Name
         self.Address = Address
-        
     }
 }
 
